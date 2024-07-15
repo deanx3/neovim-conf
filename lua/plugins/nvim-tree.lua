d0204964e186bdd88a910b00124e2a0936cd42e6
Danial Ghazali <danialghazali31@gmail.com>
2024-07-16 00:54:44 +0300

    still working

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -50,7 +50,13 @@ return {
     	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	-- or                              , branch = '0.1.x',
      	 dependencies = { 'nvim-lua/plenary.nvim' }
-    	}
+    	},
+
+{
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+}
 }
 
 
