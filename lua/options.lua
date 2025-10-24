fc1a84b1ae56ca5f98e4b9b06767bbc7eb259720
Danial Ghazali <danialghazali31@gmail.com>
2025-10-24 23:14:05 +0300

    S

--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,8 @@ local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+
